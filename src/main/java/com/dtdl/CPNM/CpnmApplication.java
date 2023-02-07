cba7719c29735caf04773d3de0cbbe88d42c0af9
koushal pandey <koushal.pandey@telekom-digital.com>
2023-02-07 14:51:49 +0530

    add repository

---
+++ src/main/java/com/dtdl/CPNM/CpnmApplication.java
@@ -0,0 +1,13 @@
+package com.dtdl.CPNM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpnmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpnmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dtdl/CPNM/CpnmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dtdl.CPNM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpnmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
